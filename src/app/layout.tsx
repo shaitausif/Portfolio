21968211344593b782b7397aae15995c869ee676
Tausif Shaikh <shaikhtausif089@gmail.com>
2025-04-18 16:50:37 +0530

    Fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Fira_Code } from "next/font/google";
-
 import "./globals.css";
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-  display: "swap",
-});
-
-const firaCode = Fira_Code({
-  variable: "--font-fira",
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "Tausif's Portfolio",
@@ -26,9 +12,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} ${firaCode.variable} antialiased text-white min-h-screen bg-[rgb(36,36,36)] z-0`}
-      >
+      <head>
+        {/* Link to Google Fonts (Roboto, Inter, Fira Code) */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Fira+Code:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="antialiased text-white min-h-screen bg-[rgb(36,36,36)] z-0 font-roboto">
         {children}
       </body>
     </html>
